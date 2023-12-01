109e204a1e3c8e3f43e679eef6f43aaad82cea5a
Kimjongwon1 <amricanoblack@naver.com>
2023-12-01 12:10:41 +0900

    Student_and_Teacher_Table_making

---
+++ queries/DDL.sql
@@ -0,0 +1,24 @@
+CREATE TABLE `student` (
+    `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `registration_number` VARCHAR(50) NOT NULL UNIQUE,
+    `phone_number` VARCHAR(50) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `major` ENUM('전공', '비전공') NOT NULL,
+    `generation` VARCHAR(50) NOT NULL,
+    `status` ENUM('수강', '졸업', '제적') NOT NULL
+);
+CREATE TABLE `teacher` (
+    `id` BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50) NOT NULL,
+    `registration_number` VARCHAR(50) NOT NULL UNIQUE,
+    `phone_number` VARCHAR(50) NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `experience` VARCHAR(255) NOT NULL,
+    `salary` INT NOT NULL,
+    `contract_start` DATE NOT NULL,
+    `contract_end` DATE NOT NULL,
+    `status` ENUM('등록', '휴직', '계약종료') NOT NULL
+);
